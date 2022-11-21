e9a0fc2b0cd27d82f0f26463d217d3a68c1ca33f
TynanGuenther <tgunther701@gmail.com>
2022-11-21 16:09:42 -0600

    Adding functionality to database

--- db.nim
+++ db.nim
@@ -1,9 +1,20 @@
-import db_sqlite, math
+import db_sqlite, math, strformat
 
-let db = open("blog.db","","","")
 
-doAssert db.getAllRows(sql"SELECT title FROM Posts") == @[Row(@["hello"])]
+#doAssert db.getAllRows(sql"SELECT title FROM Posts") == @[Row(@["NOPe"])]
 
-#proc getAllPostsTitles():seq[Row]=
+proc getAllPostsTitles*():seq[Row]=
+  let db = open("blog.db","","","")
+  result = db.getAllRows(sql"SELECT title FROM Posts")
+  db.close()
 
+proc getPostFromID*(id:int):Row=
+  let db = open("blog.db","","","")
+  result = db.getRow(sql(&"SELECT * FROM Posts WHERE ID = {id}"))
+  db.close()
+
+proc putPost*(title:string, content:string)=
+  let db = open("blog.db","","","")
+  db.exec(sql(&"INSERT INTO Posts (title, content, date) VALUES ('{title}', '{content}', date('now'))"))
+  db.close()
 
